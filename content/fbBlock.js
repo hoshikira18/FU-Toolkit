6ec171d818ca5b17e09d7b8e451e9ad465aafea1
tranvankhuyen <khuyentvhe187268@fpt.edu.vn>
2023-10-24 00:55:59 +0700

    update: facebook block handle, add icons

--- content/fbBlock.js
+++ content/fbBlock.js
@@ -1,3 +1,19 @@
+chrome.runtime.onMessage.addListener(gotMessage)
+const isBlock = localStorage.getItem("isBlock");
+console.log(isBlock);
 
-document.body.innerHTML = `<div></div>`
-alert("Học đi đừng chơi nữa!")
+function gotMessage (message) {
+    if(message) {
+        console.log(message)
+        localStorage.setItem("isBlock", message.isBlock)
+    }
+}
+
+if(isBlock == 'true') {
+    document.body.innerHTML = `<div></div>`
+    alert("Bỏ đi mà làm người")
+}
+else {
+    alert("Bỏ đi mà làm người")
+}
+
--- popup.js
+++ popup.js
@@ -1,25 +1,48 @@
 const btnVote = document.querySelector(".btn-vote");
 const btnCheat = document.querySelector(".btn-cheat");
-const btnBlockFb = document.querySelector(".fb-block");
+const inputCheckBlock = document.querySelector(".input-checkbox-block");
+
 btnVote.addEventListener("click", vote);
 btnCheat.addEventListener("click", cheat);
 
 
+inputCheckBlock.addEventListener("change", blockSocialMedia);
 
+let isBlock = localStorage.getItem("isBlock");
 
+inputCheckBlock.checked = isBlock == "true" ? true : false;
 
-btnBlockFb.addEventListener("click", (e) => {
+function blockSocialMedia(e) {
+    const params = {
+        active: true,
+        currentWindow: true
+    }
+    chrome.tabs.query(params, (tab) => {
+        console.log(tab[0].title)
+        if(tab[0].title === "Facebook") {
+            isBlock = e.target.checked;
+            localStorage.setItem("isBlock", e.target.checked);
+            sendMessage({ action: "blockSocialMedia", isBlock: isBlock })
+        }
+        else {
+            const para = document.createElement("p");
+            const node = document.createTextNode("Failed: Truy cập Facebook trước khi click!");
 
-})
+            para.appendChild(node);
+            document.querySelector(".fb-block").appendChild(para);
+        }
+    })
+
+}
 
 function sendMessage(msg) {
     const params = {
         active: true,
         currentWindow: true
     }
-    chrome.tabs.query(params, gotTab)
+    chrome.tabs.query(params, send)
 
-    function gotTab(tab) {
+    function send(tab) {
         chrome.tabs.sendMessage(tab[0].id, msg)
     }
 }
