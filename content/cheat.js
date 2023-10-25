d42fd1fca10a6a9b13ce7f12e66152d77ff79605
tranvankhuyen <khuyentvhe187268@fpt.edu.vn>
2023-10-25 10:43:18 +0700

    encrypt js code

--- content/cheat.js
+++ content/cheat.js
@@ -1,68 +1,70 @@
-async function cheat(pin, type) {
-    // Different API endpoints for different types of websites
-    const PIN = (type == "quizizz") ? "/answers?pin=" + pin : "/answers/" + pin
+// async function cheat(pin, type) {
+//     // Different API endpoints for different types of websites
+//     const PIN = (type == "quizizz") ? "/answers?pin=" + pin : "/answers/" + pin
 
-    const endPoints = "https://api.quizit.online/" + type + PIN 
+//     const endPoints = "https://api.quizit.online/" + type + PIN 
 
-    const response = await fetch(endPoints)
-    const data = await response.json()
-    let answers = data.data
-    answers ? alert("Success!") : alert("Error: Invalid pin")
-    if (type == "quizizz") {
-        answers = data.data.answers
-    }
-    else if (type == "blooket") {
-        answers = data.data
-    }
-    setInterval(() => {
-        for (let i = 0; i < answers.length; i++) {
-            let classQuestion;
+//     const response = await fetch(endPoints)
+//     const data = await response.json()
+//     let answers = data.data
+//     answers ? alert("Success!") : alert("Error: Invalid pin")
+//     if (type == "quizizz") {
+//         answers = data.data.answers
+//     }
+//     else if (type == "blooket") {
+//         answers = data.data
+//     }
+//     setInterval(() => {
+//         for (let i = 0; i < answers.length; i++) {
+//             let classQuestion;
 
-            // Different question class names for different types of websites
-            if(type == "quizizz") {
-                classQuestion = ".question-text"
-            } else if(type == "blooket") {
-                classQuestion = ".styles__questionText___2MlSZ-camelCase"
-            }
+//             // Different question class names for different types of websites
+//             if(type == "quizizz") {
+//                 classQuestion = ".question-text"
+//             } else if(type == "blooket") {
+//                 classQuestion = ".styles__questionText___2MlSZ-camelCase"
+//             }
 
-            const questionHTML = document.querySelector(classQuestion).textContent
+//             const questionHTML = document.querySelector(classQuestion).textContent
 
-            let questionAPI  = answers[i].question.text
-            // Remove HTML tags from questionAPI
-            while(questionAPI.includes("<p>") || questionAPI.includes("</p>") || questionAPI.includes("<u>") || questionAPI.includes("</u>")) {
-                questionAPI = questionAPI.replace("<p>", "").replace("</p>", "").replace("</u>", "").replace("<u>", "")
-            }
-            if (questionHTML == questionAPI) {
-                let options;
-                // Different option class names for different types of websites
-                if(type == "quizizz") {
-                    options = document.querySelectorAll(".option-inner")
-                } else if(type == "blooket") {
-                    options = document.querySelectorAll(".styles__answerTextContainer___3YgCT-camelCase")
-                }
+//             let questionAPI  = answers[i].question.text
+//             // Remove HTML tags from questionAPI
+//             while(questionAPI.includes("<p>") || questionAPI.includes("</p>") || questionAPI.includes("<u>") || questionAPI.includes("</u>")) {
+//                 questionAPI = questionAPI.replace("<p>", "").replace("</p>", "").replace("</u>", "").replace("<u>", "")
+//             }
+//             if (questionHTML == questionAPI) {
+//                 let options;
+//                 // Different option class names for different types of websites
+//                 if(type == "quizizz") {
+//                     options = document.querySelectorAll(".option-inner")
+//                 } else if(type == "blooket") {
+//                     options = document.querySelectorAll(".styles__answerTextContainer___3YgCT-camelCase")
+//                 }
 
-                for(let j = 0; j < options.length; j++) {
+//                 for(let j = 0; j < options.length; j++) {
 
-                    const optionHTML = options[j].textContent
-                    // click answers
-                    for(let k = 0; k < answers[i].answers.length; k++) {
-                        // Remove HTML tags from optionAPI
-                        const optionAPI = answers[i].answers[k].text.replace("<p>", "").replace("</p>", "").replace("</u>", "").replace("<u>", "").replace("<p>", "").replace("</p>", "").replace("</u>", "").replace("<u>", "")
-                        if(optionHTML == optionAPI) {
-                            options[j].click();
-                            console.log("Clicked: " + options[j].textContent)
-                        }  
-                    } 
-                }
-            }
-        }
-    }, 2000)
-}
+//                     const optionHTML = options[j].textContent
+//                     // click answers
+//                     for(let k = 0; k < answers[i].answers.length; k++) {
+//                         // Remove HTML tags from optionAPI
+//                         const optionAPI = answers[i].answers[k].text.replace("<p>", "").replace("</p>", "").replace("</u>", "").replace("<u>", "").replace("<p>", "").replace("</p>", "").replace("</u>", "").replace("<u>", "")
+//                         if(optionHTML == optionAPI) {
+//                             options[j].click();
+//                             console.log("Clicked: " + options[j].textContent)
+//                         }  
+//                     } 
+//                 }
+//             }
+//         }
+//     }, 2000)
+// }
 
-chrome.runtime.onMessage.addListener((message) => {
-        if(message.pin) {
-            console.log("Sent successfully. Pin: " + message.pin + " Type: " + message.type)
-        }
-        cheat(message.pin, message.type);
-})
+// chrome.runtime.onMessage.addListener((message) => {
+//         if(message.pin) {
+//             console.log("Sent successfully. Pin: " + message.pin + " Type: " + message.type)
+//         }
+//         cheat(message.pin, message.type);
+// })
 
+
+const _0x438f1a=_0x350c;function _0x4252(){const _0xbf8833=['/answers?p','ybNOb','</p>','131964HJuUHk','KlnRk','Error:\x20Inv','KvMDz','torAll','nline/','includes','pin','question','log','VmJNa','BDmrh','TmTOC','.option-in','qyzSt','.styles__q','runtime','2388078ZxdSoX','.question-','pvYgX','UKAdb','ner','cxuZp','blooket','CeyMv','2OkHMmH','vltBl','json','t___2MlSZ-','glWOF','obYyG','Sent\x20succe','nhvpo','bqfqC','querySelec','yMXbh','addListene','/answers/','LReoz','elCase','WDaVp','tklSM','<p>','QCySQ','data','ajxjn','in:\x20','Success!','_3YgCT-cam','alid\x20pin','bBqOh','vJBYK','onMessage','ssfully.\x20P','uestionTex','NISqu','in=','type','Clicked:\x20','textConten','.styles__a','1644784SyxgMv','tor','text','19003290eoBdBW','NEBMu','nswerTextC','i.quizit.o','AuRDx','RUXEu','GhmtF','6579lRstJj','pmMlE','\x20Type:\x20','https://ap','click','235826KRjLJs','255AciNqa','CohYD','vVDGp','camelCase','answers','quizizz','850923OGaWyP','length','sasIK','replace','<u>','1608txeEss','ZtZCL','</u>','ontainer__'];_0x4252=function(){return _0xbf8833;};return _0x4252();}(function(_0x10afde,_0x2f5c0d){const _0x4af6b0=_0x350c,_0xa7482f=_0x10afde();while(!![]){try{const _0x50f24c=parseInt(_0x4af6b0(0x1cc))/(0xff1+-0x2122+0x8e*0x1f)+-parseInt(_0x4af6b0(0x1f8))/(0x170*-0xc+-0x13ae+-0x2*-0x1278)*(parseInt(_0x4af6b0(0x1d3))/(0x1c6+0x187a+-0x8bf*0x3))+-parseInt(_0x4af6b0(0x1bd))/(-0x2129+0x431+0x2*0xe7e)+parseInt(_0x4af6b0(0x1cd))/(0x1dd0+0x25f*0x1+-0x202a)*(-parseInt(_0x4af6b0(0x1df))/(-0x8*-0x202+-0xb98+-0x2*0x239))+parseInt(_0x4af6b0(0x1f0))/(0x1*0x1fbf+0x1c15+0x3f*-0xf3)+parseInt(_0x4af6b0(0x1d8))/(0x9ca*-0x3+0x30*-0x17+-0x21b6*-0x1)*(parseInt(_0x4af6b0(0x1c7))/(0xdaa+0x259c*0x1+-0x333d))+parseInt(_0x4af6b0(0x1c0))/(-0xcac+-0x384*0x1+0x103a);if(_0x50f24c===_0x2f5c0d)break;else _0xa7482f['push'](_0xa7482f['shift']());}catch(_0x116a70){_0xa7482f['push'](_0xa7482f['shift']());}}}(_0x4252,-0xe7*-0x147e+-0x12da41+0xcafac));function _0x350c(_0x3e2723,_0x46c283){const _0x936221=_0x4252();return _0x350c=function(_0x3e08b1,_0x3b3ac5){_0x3e08b1=_0x3e08b1-(0x1*0x1917+0x596*-0x2+-0xc37);let _0xb941da=_0x936221[_0x3e08b1];return _0xb941da;},_0x350c(_0x3e2723,_0x46c283);}async function cheat(_0x450d7e,_0x4c58a0){const _0x59e0f7=_0x350c,_0x475264={'VmJNa':function(_0xd90980,_0x2ced0f){return _0xd90980<_0x2ced0f;},'KlnRk':function(_0x342efb,_0xff1d52){return _0x342efb==_0xff1d52;},'glWOF':_0x59e0f7(0x1d2),'CeyMv':_0x59e0f7(0x1f1)+_0x59e0f7(0x1bf),'yMXbh':function(_0x1f9d6c,_0x3538ef){return _0x1f9d6c==_0x3538ef;},'AuRDx':_0x59e0f7(0x1f6),'ajxjn':_0x59e0f7(0x1ee)+_0x59e0f7(0x1b6)+_0x59e0f7(0x1fb)+_0x59e0f7(0x1d0),'vJBYK':_0x59e0f7(0x209),'sasIK':_0x59e0f7(0x1de),'ybNOb':_0x59e0f7(0x1d7),'NISqu':_0x59e0f7(0x1da),'RUXEu':_0x59e0f7(0x1ec)+_0x59e0f7(0x1f4),'QCySQ':function(_0x52bbd3,_0x2a49c7){return _0x52bbd3==_0x2a49c7;},'obYyG':_0x59e0f7(0x1bc)+_0x59e0f7(0x1c2)+_0x59e0f7(0x1db)+_0x59e0f7(0x20f)+_0x59e0f7(0x206),'TmTOC':function(_0x4fede2,_0x3cc1d4){return _0x4fede2<_0x3cc1d4;},'UKAdb':function(_0x41780f,_0x391b13){return _0x41780f<_0x391b13;},'bBqOh':function(_0x19010c,_0x1b3428){return _0x19010c+_0x1b3428;},'qyzSt':_0x59e0f7(0x1ba),'LReoz':function(_0x5f2ae0,_0x55d801){return _0x5f2ae0+_0x55d801;},'vVDGp':_0x59e0f7(0x1dc)+_0x59e0f7(0x1b8),'vltBl':_0x59e0f7(0x204),'pvYgX':function(_0xbbd45d,_0x13da52){return _0xbbd45d+_0x13da52;},'nhvpo':_0x59e0f7(0x1ca)+_0x59e0f7(0x1c3)+_0x59e0f7(0x1e4),'GhmtF':function(_0x497629,_0x1082f6){return _0x497629(_0x1082f6);},'ZtZCL':_0x59e0f7(0x20e),'BDmrh':function(_0x45247c,_0x417a61){return _0x45247c(_0x417a61);},'bqfqC':_0x59e0f7(0x1e1)+_0x59e0f7(0x210),'tklSM':function(_0x56b633,_0x426ca7,_0x4705bc){return _0x56b633(_0x426ca7,_0x4705bc);}},_0x21a9d4=_0x475264[_0x59e0f7(0x202)](_0x4c58a0,_0x475264[_0x59e0f7(0x1fc)])?_0x475264[_0x59e0f7(0x205)](_0x475264[_0x59e0f7(0x1cf)],_0x450d7e):_0x475264[_0x59e0f7(0x205)](_0x475264[_0x59e0f7(0x1f9)],_0x450d7e),_0x1dec21=_0x475264[_0x59e0f7(0x211)](_0x475264[_0x59e0f7(0x1f2)](_0x475264[_0x59e0f7(0x1ff)],_0x4c58a0),_0x21a9d4),_0x3c1107=await _0x475264[_0x59e0f7(0x1c6)](fetch,_0x1dec21),_0x5b34e4=await _0x3c1107[_0x59e0f7(0x1fa)]();let _0xe6874e=_0x5b34e4[_0x59e0f7(0x20b)];_0xe6874e?_0x475264[_0x59e0f7(0x1c6)](alert,_0x475264[_0x59e0f7(0x1d9)]):_0x475264[_0x59e0f7(0x1ea)](alert,_0x475264[_0x59e0f7(0x200)]);if(_0x475264[_0x59e0f7(0x202)](_0x4c58a0,_0x475264[_0x59e0f7(0x1fc)]))_0xe6874e=_0x5b34e4[_0x59e0f7(0x20b)][_0x59e0f7(0x1d1)];else _0x475264[_0x59e0f7(0x20a)](_0x4c58a0,_0x475264[_0x59e0f7(0x1c4)])&&(_0xe6874e=_0x5b34e4[_0x59e0f7(0x20b)]);_0x475264[_0x59e0f7(0x208)](setInterval,()=>{const _0xcd0b5f=_0x59e0f7;for(let _0x12c692=0xf50+-0x111d+0x1cd;_0x475264[_0xcd0b5f(0x1e9)](_0x12c692,_0xe6874e[_0xcd0b5f(0x1d4)]);_0x12c692++){let _0x51534d;if(_0x475264[_0xcd0b5f(0x1e0)](_0x4c58a0,_0x475264[_0xcd0b5f(0x1fc)]))_0x51534d=_0x475264[_0xcd0b5f(0x1f7)];else _0x475264[_0xcd0b5f(0x202)](_0x4c58a0,_0x475264[_0xcd0b5f(0x1c4)])&&(_0x51534d=_0x475264[_0xcd0b5f(0x20c)]);const _0x58f7c3=document[_0xcd0b5f(0x201)+_0xcd0b5f(0x1be)](_0x51534d)[_0xcd0b5f(0x1bb)+'t'];let _0x560dcd=_0xe6874e[_0x12c692][_0xcd0b5f(0x1e7)][_0xcd0b5f(0x1bf)];while(_0x560dcd[_0xcd0b5f(0x1e5)](_0x475264[_0xcd0b5f(0x212)])||_0x560dcd[_0xcd0b5f(0x1e5)](_0x475264[_0xcd0b5f(0x1d5)])||_0x560dcd[_0xcd0b5f(0x1e5)](_0x475264[_0xcd0b5f(0x1dd)])||_0x560dcd[_0xcd0b5f(0x1e5)](_0x475264[_0xcd0b5f(0x1b7)])){_0x560dcd=_0x560dcd[_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x212)],'')[_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x1d5)],'')[_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x1b7)],'')[_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x1dd)],'');}if(_0x475264[_0xcd0b5f(0x1e0)](_0x58f7c3,_0x560dcd)){let _0x498565;if(_0x475264[_0xcd0b5f(0x202)](_0x4c58a0,_0x475264[_0xcd0b5f(0x1fc)]))_0x498565=document[_0xcd0b5f(0x201)+_0xcd0b5f(0x1e3)](_0x475264[_0xcd0b5f(0x1c5)]);else _0x475264[_0xcd0b5f(0x20a)](_0x4c58a0,_0x475264[_0xcd0b5f(0x1c4)])&&(_0x498565=document[_0xcd0b5f(0x201)+_0xcd0b5f(0x1e3)](_0x475264[_0xcd0b5f(0x1fd)]));for(let _0x5e7c45=0xa8f+0x1299*-0x1+0x80a*0x1;_0x475264[_0xcd0b5f(0x1eb)](_0x5e7c45,_0x498565[_0xcd0b5f(0x1d4)]);_0x5e7c45++){const _0x24cbad=_0x498565[_0x5e7c45][_0xcd0b5f(0x1bb)+'t'];for(let _0x5b3921=0x1a45+-0x1ff*-0xa+-0x2e3b;_0x475264[_0xcd0b5f(0x1f3)](_0x5b3921,_0xe6874e[_0x12c692][_0xcd0b5f(0x1d1)][_0xcd0b5f(0x1d4)]);_0x5b3921++){const _0x3927ef=_0xe6874e[_0x12c692][_0xcd0b5f(0x1d1)][_0x5b3921][_0xcd0b5f(0x1bf)][_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x212)],'')[_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x1d5)],'')[_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x1b7)],'')[_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x1dd)],'')[_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x212)],'')[_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x1d5)],'')[_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x1b7)],'')[_0xcd0b5f(0x1d6)](_0x475264[_0xcd0b5f(0x1dd)],'');_0x475264[_0xcd0b5f(0x20a)](_0x24cbad,_0x3927ef)&&(_0x498565[_0x5e7c45][_0xcd0b5f(0x1cb)](),console[_0xcd0b5f(0x1e8)](_0x475264[_0xcd0b5f(0x211)](_0x475264[_0xcd0b5f(0x1ed)],_0x498565[_0x5e7c45][_0xcd0b5f(0x1bb)+'t'])));}}}}},-0x226b+-0x12f6+0x3d31);}chrome[_0x438f1a(0x1ef)][_0x438f1a(0x1b4)][_0x438f1a(0x203)+'r'](_0x4719fb=>{const _0x4f98f5=_0x438f1a,_0x4200b3={'NEBMu':function(_0x261384,_0x5dbf1e){return _0x261384+_0x5dbf1e;},'KvMDz':function(_0x5cb4b2,_0x2fa740){return _0x5cb4b2+_0x2fa740;},'CohYD':function(_0x7bb21c,_0x2afa72){return _0x7bb21c+_0x2afa72;},'pmMlE':_0x4f98f5(0x1fe)+_0x4f98f5(0x1b5)+_0x4f98f5(0x20d),'WDaVp':_0x4f98f5(0x1c9),'cxuZp':function(_0x1c6420,_0x2b1d57,_0x546af6){return _0x1c6420(_0x2b1d57,_0x546af6);}};_0x4719fb[_0x4f98f5(0x1e6)]&&console[_0x4f98f5(0x1e8)](_0x4200b3[_0x4f98f5(0x1c1)](_0x4200b3[_0x4f98f5(0x1e2)](_0x4200b3[_0x4f98f5(0x1ce)](_0x4200b3[_0x4f98f5(0x1c8)],_0x4719fb[_0x4f98f5(0x1e6)]),_0x4200b3[_0x4f98f5(0x207)]),_0x4719fb[_0x4f98f5(0x1b9)])),_0x4200b3[_0x4f98f5(0x1f5)](cheat,_0x4719fb[_0x4f98f5(0x1e6)],_0x4719fb[_0x4f98f5(0x1b9)]);});
